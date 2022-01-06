a865d0caa8342a6637de7feea3507e85c2021b04
Oksana <ksiuha-92@mail.ru>
2022-01-06 15:54:17 +0300

    Добавлена возможность оставлять значение поля по умолчанию

--- addressbook-web-tests/src/test/java/edu/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/edu/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package edu.addressbook.tests;
 
 import edu.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
