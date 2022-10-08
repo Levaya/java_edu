83b3b27a1e5fe8db0fb379efc63aedcfd55135a3
Oksana <ksiuha-92@mail.ru>
2022-10-08 16:00:07 +0300

    Добавлена генерация тестовых данных в json

--- addressbook-web-tests/src/test/java/edu/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/edu/addressbook/model/GroupData.java
@@ -1,5 +1,6 @@
 package edu.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -10,9 +11,12 @@ import java.util.Objects;
 public class GroupData{
 
     @XStreamOmitField
-    private int id = Integer.MAX_VALUE;;
+    private int id = Integer.MAX_VALUE;
+    @Expose
     private String name;
+    @Expose
     private String header;
+    @Expose
     private String footer;
 
     public GroupData withId(int id) {
